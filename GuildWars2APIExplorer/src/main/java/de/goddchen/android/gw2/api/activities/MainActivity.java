7310bbb9a0d83f3d7c1e85d458613d04f4eb573d
Goddchen <goddchen@googlemail.com>
2013-05-23 18:16:28 +0200

    add tablet layout

--- GuildWars2APIExplorer/src/main/java/de/goddchen/android/gw2/api/activities/MainActivity.java
+++ GuildWars2APIExplorer/src/main/java/de/goddchen/android/gw2/api/activities/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.crittercism.app.Crittercism;
 import de.goddchen.android.gw2.api.R;
+import de.goddchen.android.gw2.api.fragments.ItemSearchFragment;
+import de.goddchen.android.gw2.api.fragments.MatchesFragment;
+import de.goddchen.android.gw2.api.fragments.RecipeSearchFragment;
+import de.goddchen.android.gw2.api.fragments.WorldsFragment;
 import de.goddchen.android.gw2.api.fragments.dialogs.CrashDialogFragment;
 
 public class MainActivity extends SherlockFragmentActivity implements View.OnClickListener {
@@ -25,14 +29,34 @@ public class MainActivity extends SherlockFragmentActivity implements View.OnCli
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.items) {
-            startActivity(new Intent(getApplicationContext(), ItemsActivity.class));
-        } else if (view.getId() == R.id.events) {
-            startActivity(new Intent(getApplicationContext(), EventsActivity.class));
-        } else if (view.getId() == R.id.wvwvw) {
-            startActivity(new Intent(getApplicationContext(), WvWActivity.class));
-        } else if (view.getId() == R.id.recipes) {
-            startActivity(new Intent(getApplicationContext(), RecipesActivity.class));
+        if (findViewById(R.id.fragment) == null) {
+            if (view.getId() == R.id.items) {
+                startActivity(new Intent(getApplicationContext(), ItemsActivity.class));
+            } else if (view.getId() == R.id.events) {
+                startActivity(new Intent(getApplicationContext(), EventsActivity.class));
+            } else if (view.getId() == R.id.wvwvw) {
+                startActivity(new Intent(getApplicationContext(), WvWActivity.class));
+            } else if (view.getId() == R.id.recipes) {
+                startActivity(new Intent(getApplicationContext(), RecipesActivity.class));
+            }
+        } else {
+            if (view.getId() == R.id.items) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment, ItemSearchFragment.newInstance())
+                        .commit();
+            } else if (view.getId() == R.id.events) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment, WorldsFragment.newInstance())
+                        .commit();
+            } else if (view.getId() == R.id.wvwvw) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment, MatchesFragment.newInstance())
+                        .commit();
+            } else if (view.getId() == R.id.recipes) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment, RecipeSearchFragment.newInstance())
+                        .commit();
+            }
         }
     }
 }
