2f05e9c406355b37dcd5b17b76c0ec3316e7c6e3
Goddchen <goddchen@googlemail.com>
2013-08-10 14:28:00 +0200

    show icons of ingredients

--- GuildWars2APIExplorer/src/main/java/de/goddchen/android/gw2/api/adapter/IngredientAdapter.java
+++ GuildWars2APIExplorer/src/main/java/de/goddchen/android/gw2/api/adapter/IngredientAdapter.java
@@ -1,6 +1,5 @@
 package de.goddchen.android.gw2.api.adapter;
 
-import android.R;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +9,13 @@ import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 
 import java.util.List;
 import java.util.Locale;
 
+import de.goddchen.android.gw2.api.R;
 import de.goddchen.android.gw2.api.activities.BaseFragmentActivity;
 import de.goddchen.android.gw2.api.async.GsonRequest;
 import de.goddchen.android.gw2.api.data.Item;
@@ -24,17 +26,19 @@ import de.goddchen.android.gw2.api.data.Recipe;
  */
 public class IngredientAdapter extends ArrayAdapter<Recipe.Ingredient> {
     public IngredientAdapter(Context context, List<Recipe.Ingredient> objects) {
-        super(context, R.layout.simple_list_item_1, objects);
+        super(context, android.R.layout.simple_list_item_1, objects);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         final Recipe.Ingredient ingredient = getItem(position);
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.simple_list_item_1,
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.listitem_ingredient,
                     parent, false);
         }
-        final TextView textView = (TextView) convertView.findViewById(R.id.text1);
+        final TextView textView = (TextView) convertView.findViewById(R.id.name);
+        final NetworkImageView icon = (NetworkImageView) convertView.findViewById(R.id.icon);
+        icon.setDefaultImageResId(android.R.drawable.ic_menu_gallery);
         textView.setText(de.goddchen.android.gw2.api.R.string.loading);
         ((BaseFragmentActivity) getContext()).getRequestQueue()
                 .add(new GsonRequest<Item>(
@@ -47,6 +51,10 @@ public class IngredientAdapter extends ArrayAdapter<Recipe.Ingredient> {
                             public void onResponse(Item item) {
                                 textView.setText(String.format("%d x %s",
                                         ingredient.count, item.name));
+                                icon.setImageUrl(String.format("https://render.guildwars2.com/file/%s/%s.png",
+                                        item.icon_file_signature, item.icon_file_id),
+                                        new ImageLoader(((BaseFragmentActivity) getContext()).getRequestQueue(),
+                                                ((BaseFragmentActivity) getContext()).getImageCache()));
                             }
                         },
                         new Response.ErrorListener() {
