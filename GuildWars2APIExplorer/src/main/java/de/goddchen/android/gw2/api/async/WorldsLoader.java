f350834641ce97601a6c29228da1020e0a5d71dd
Goddchen <goddchen@googlemail.com>
2014-10-25 14:45:14 +0200

    use v2 worlds API

--- GuildWars2APIExplorer/src/main/java/de/goddchen/android/gw2/api/async/WorldsLoader.java
+++ GuildWars2APIExplorer/src/main/java/de/goddchen/android/gw2/api/async/WorldsLoader.java
@@ -2,18 +2,21 @@ package de.goddchen.android.gw2.api.async;
 
 import android.content.Context;
 import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import de.goddchen.android.gw2.api.Application;
-import de.goddchen.android.gw2.api.data.World;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import de.goddchen.android.gw2.api.Application;
+import de.goddchen.android.gw2.api.data.World;
 
 /**
  * Created by Goddchen on 22.05.13.
@@ -29,7 +32,7 @@ public class WorldsLoader extends FixedAsyncTaskLoader<List<World>> {
             List<World> worlds = Application.getDatabaseHelper().getWorldDao().queryForAll();
             if (worlds == null || worlds.isEmpty()) {
                 HttpsURLConnection connection =
-                        (HttpsURLConnection) new URL("https://api.guildwars2.com/v1/world_names.json?lang="
+                        (HttpsURLConnection) new URL("https://api.guildwars2.com/v2/worlds?ids=all&lang="
                                 + Locale.getDefault().getLanguage())
                                 .openConnection();
                 worlds = new Gson().fromJson(new InputStreamReader(connection.getInputStream()),
